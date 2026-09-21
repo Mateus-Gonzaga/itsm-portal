104764a85bdcfc24d28c523f34bf4f2c921c3f23
Mateus Gonzaga <v001mateus@gmail.com>
2026-09-21 16:53:17 -0300

    fix: corrigir declaracao de variaveis no model TicketTaskGoogleEvent

--- app/Models/TicketTaskGoogleEvent.php
+++ app/Models/TicketTaskGoogleEvent.php
@@ -6,11 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class TicketTaskGoogleEvent extends Model
 {
-    protected  = 'ticket_task_id';
-    public  = false;
-    protected  = 'int';
+    protected $primaryKey = 'ticket_task_id';
+    public $incrementing = false;
+    protected $keyType = 'int';
 
-    protected  = [
+    protected $fillable = [
         'ticket_task_id',
         'ticket_id',
         'google_event_id',
